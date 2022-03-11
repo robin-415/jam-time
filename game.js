fc9bd041dc1e01a0cf769ea31b348b675345bb45
Robin Castel Navarro <robin.castel@gmail.com>
2022-03-11 06:03:24 -0800

    revised instructions and styles css, worked on functionality

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
 // Global Variables
-const instruIcons = document.querySelector('#allInstruments')
-const instruButtons = document.querySelectorAll('.buttonImage')
 const startButton = document.querySelector('.startGame')
 const notifyMessage = document.querySelector('.message')
 const roundDisplay = document.querySelector('#nums')
-const randomNum = (Math.floor(Math.random() * 4) +1)
+// const randomNum = (Math.floor(Math.random() * 4) +1)
+
+const allInstruments = document.querySelectorAll('#allInstruments')
+const keysButton = document.getElementById('i0')
+const guitarButton = document.getElementById('i1')
+const drumsButton = document.getElementById('i2')
+const bassButton = document.getElementById('i3')
 
 let keysAudio = document.getElementById('sounds0')
 let guitarAudio = document.getElementById('sounds1')
@@ -15,62 +19,110 @@ let roundNum = 0
 let compSeq = []
 let playerSeq = []
 let gameSeq = []
-let sound = true
+let played = true
 let time = 0
 let round = []
 
+// Game Functions - Initialize Game, Player, New Round, Verify
 function game () {
   gameSeq = []
   playerSeq = []
   roundNum = 0
-  active = true
   roundDisplay.innerText = '1'
-  for (let i = 0; i < 10; i++) {
-    gameSeq.push(randomNum)
-  }
-  time = setInterval(nextRound, 1000)
+  randomSeq ()
+  time = setInterval(newRound, 500)
   }
 
-function nextRound (){
-  if (sound == round) {
-    clearInterval(time)
+function randomSeq () {
+  for (let i = 0; i < 20; i++) {
+    gameSeq.push(Math.floor(Math.random() * 4) +1)
+  }
+}  
+// Round Sequence
+// function newRound () { 
+  // game.roundNum += 1
+//   $(`#allInstruments`).addClass
+//   setTimeout(function(){
+//   $(`#allinstruments`).removeClass
+//   }, 300)
+// }
+
+// Player Turn
+
+// Computer Turn
+// function newRound() {
+//   resetGame()
+// }
+
+// function createSeq (){
+//   game.gameSeq.push(randomNum)
+// 
+
+function newRound (){
+  if (played == round) {
     compSeq = false
     playerSeq = true
+    clearInterval(time)
   }
   if (compSeq) {
-    setTimeout(() => {
-    clicks ()
-      sound++
-    }, 300)
+      keys()
+      guitar()
+      drums()
+      bass()
+    }
   }
-}
-
-function clicks() {
-if (keysAudio){
-  sound = true
-  document.getElementById('i0').style.borderColor = 'yellow'
-}
-if (guitarAudio){
-  sound = true
-  document.getElementById('i1').style.borderColor = 'yellow'
-}
-if (drumsAudio){
-  sound = true
-  document.getElementById('i2').style.borderColor = 'yellow'
-}
-if (bassAudio){
-  sound = true
-  document.getElementById('i3').style.borderColor = 'yellow'
-}
-}
 
 function verifyRound () {
-  if (playerSeq.length === compSeq.length) {
-    notifyMessage.innerHTML = 'Winner!'
-    if (playerSeq[playerSeq.length - 1] !== gameSeq[playSeq.length - 1])
-    active = false
+if (playerSeq.length === compSeq.length) {
+      notifyMessage.innerHTML = 'Winner!'
+      if (playerSeq[playerSeq.length - 1] !== gameSeq[playerSeq.length - 1])
+      played = false
+    }
   }
+  
+
+// function resetGame() {
+//   game.gameSeq = []
+//   game.roundNum = 0
+//   playRound()
+// }
+
+// Button Functions - Audio, Border Color
+function keys() {
+  if (played) {
+  keysButton.style.borderColor = 'yellow'
+  played = true
+  keysAudio.play()
+  }
+  setTimeout(300)
 }
+
+function guitar() {
+  if (played) {
+  guitarButton.style.borderColor = 'yellow'
+  played = true
+  guitarAudio.play()
+  }
+  setTimeout(300)
+}
+
+function drums() {
+  if (played) {
+  drumsButton.style.borderColor = 'yellow'
+  played = true
+  drumsAudio.play()
+  }
+  setTimeout(300)
+}
+function bass() {
+  if (played) {
+  bassButton.style.borderColor = 'yellow'
+  played = true
+  bassAudio.play()
+  }
+  setTimeout(300)
+}
+
 
 // Game Over
 // function gameOver () {
@@ -81,11 +133,3 @@ function verifyRound () {
 startButton.addEventListener('click', (event) => {
   game()
   })
-
-
-// instruButtons.addEventListener('click', (event) => {
-//   playerSeq.push()
-//   clicks()
-//   verifyRound()
-// }
-// )
